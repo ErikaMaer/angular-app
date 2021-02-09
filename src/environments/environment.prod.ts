8520fa6fe73262318ebdf8c6b9a0e2672e526fa7
Erika Maer <erikamaer00@gmail.com>
2021-02-09 21:39:37 +0300

    FireBase and error processing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyAb838f5pm57nBfTquniysgBvhoPecdlUc'
 };
