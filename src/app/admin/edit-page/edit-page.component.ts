b2c32212dee86284b748482fa94a133134eaa8a3
Erika Maer <erikamaer00@gmail.com>
2021-02-17 00:20:57 +0300

    Message plugin and ruLocale

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -5,6 +5,7 @@ import {PostsService} from '../../shared/posts.service';
 import {switchMap} from 'rxjs/operators';
 import {Post} from '../../shared/interfaces';
 import {Subscription} from 'rxjs';
+import {AlertService} from '../shared/services/alert.service';
 
 @Component({
   selector: 'app-edit-page',
@@ -20,7 +21,8 @@ export class EditPageComponent implements OnInit, OnDestroy {
 
   constructor(
     private route: ActivatedRoute,
-    private postsService: PostsService
+    private postsService: PostsService,
+    private alert: AlertService
     ) { }
 
   ngOnInit(){
@@ -53,6 +55,7 @@ export class EditPageComponent implements OnInit, OnDestroy {
       title: this.form.value.title
     }).subscribe(() => {
       this.submitted = false;
+      this.alert.success('Пост был обновлен');
     });
   }
 }
